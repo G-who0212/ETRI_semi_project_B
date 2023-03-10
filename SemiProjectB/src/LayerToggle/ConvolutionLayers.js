b47c7c3b502167882936c3bd1f0a6af75de2f78b
p6trick <juniyook@korea.ac.kr>
2023-03-10 15:36:36 +0900

    Third

---
+++ SemiProjectB/src/LayerToggle/ConvolutionLayers.js
@@ -0,0 +1,16 @@
+
+const ConvolutionLayers = () => {
+
+    return (
+        <div className="ConvolutionLayers">
+            <ul >
+                <li><button>Conv2d</button></li> 
+                <li><button>Conv1d</button></li>
+            </ul>
+        </div>
+
+    );
+
+};
+
+export default ConvolutionLayers;
---
+++ SemiProjectB/src/LayerToggle/PoolingLayers.js
@@ -0,0 +1,16 @@
+
+const PoolingLayers = () => {
+
+    return (
+        <div className="PoolingLayers">
+            <ul>
+                <li><button>MaxPool</button></li> 
+                <li><button>AVGPool</button></li>
+            </ul>
+        </div>
+
+    );
+
+};
+
+export default PoolingLayers;
